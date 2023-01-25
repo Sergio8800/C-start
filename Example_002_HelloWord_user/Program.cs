299da322f273842e52aa452c4248f011fcb191e4
Sergio8800 <wasp8800@gmail.com>
2023-01-25 23:22:17 +0100

    second

---
+++ Example_002_HelloWord_user/Program.cs
@@ -0,0 +1,7 @@
+﻿// dotnet new console
+Console.WriteLine("input you name");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello");
+Console.WriteLine(userName);
+Console.Write(userName);
+Console.Write(" - it is your name");
