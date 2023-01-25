9cbc913e683bbf3bf18972a891a4a98df1e2ded3
Sergio8800 <wasp8800@gmail.com>
2023-01-25 23:49:29 +0100

    six

---
+++ Example_005_ifElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("input name");
+string name = Console.ReadLine();
+if (name == "Masha")
+{
+Console.WriteLine("Wooooy");
+}
+
+else
+{
+    Console.WriteLine("nOOOOO");
+    Console.WriteLine(name);
+                                
+}
