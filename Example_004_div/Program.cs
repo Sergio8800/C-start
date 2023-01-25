a618a71f05dc1f3294dfd0dba54c6740213b7eca
Sergio8800 <wasp8800@gmail.com>
2023-01-25 23:35:48 +0100

    four

---
+++ Example_004_div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double numbrA = 9;
+double numbrB = 10;
+double result = numbrA / numbrB;
+Console.WriteLine(result);
