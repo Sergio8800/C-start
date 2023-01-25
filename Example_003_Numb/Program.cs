831f0b3b7acee1649e8028a54c4ea1eb0e2a9a90
Sergio8800 <wasp8800@gmail.com>
2023-01-25 23:32:03 +0100

    third

---
+++ Example_003_Numb/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numbrA = 9;
+int numbrB = 10;
+int result = numbrA + numbrB;
+Console.WriteLine(result)
