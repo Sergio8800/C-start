515807f7a212c8ed5ca30439870e13cdfabb8f00
Sergio8800 <wasp8800@gmail.com>
2023-01-28 14:04:24 +0100

    home work tast 4

---
+++ HomeWork_004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число:");
+string str1 = Console.ReadLine();
+int str1_int = Convert.ToInt32(str1);
+while(str1_int>1)
+{
+    if (str1_int%2==0)
+    {
+        Console.Write(", " + str1_int.ToString());
+    }
+   str1_int = str1_int - 1; 
+}
