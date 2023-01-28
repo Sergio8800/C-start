5ae22d7698df5cbf9fe1c5cbdf5cf7e54523aebf
Sergio8800 <wasp8800@gmail.com>
2023-01-28 13:18:26 +0100

    home work tast 2

---
+++ HomeWork_002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число и нажмите клавишу Enter: ");
+string str1 = Console.ReadLine();
+int str1_int = Convert.ToInt32(str1);
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+string str2 = Console.ReadLine();
+int str2_int = Convert.ToInt32(str2);
+Console.Write("Введите третье число и нажмите клавишу Enter: ");
+string str3 = Console.ReadLine();
+int str3_int = Convert.ToInt32(str3);
+int max = str1_int;
+if (str2_int > max)
+{
+    max = str2_int;
+}
+if (str3_int > max)
+{
+    max = str3_int;
+}
+Console.WriteLine("Maximum: " + max.ToString());
