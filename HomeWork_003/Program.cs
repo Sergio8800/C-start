3aeb7341b47b1c9ad31b0149a8df637e996c5773
Sergio8800 <wasp8800@gmail.com>
2023-01-28 13:38:28 +0100

    home work task 3

---
+++ HomeWork_003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число и нажмите клавишу Enter: ");
+string str1 = Console.ReadLine();
+int str1_int = Convert.ToInt32(str1);
+if (str1_int%2 == 0)
+{
+    Console.Write("число: " + str1_int.ToString());
+    Console.WriteLine(" чётное");
+}
+else
+{
+    Console.Write("число: " + str1_int.ToString());
+    Console.WriteLine(" не чётное");
+}
