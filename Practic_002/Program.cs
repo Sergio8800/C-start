a76f523bde2628ca5e151979991e43027dd534e8
Sergio8800 <wasp8800@gmail.com>
2023-01-29 21:58:40 +0100

    week

---
+++ Practic_002/Program.cs
@@ -0,0 +1,14 @@
+﻿var dict = new Dictionary<int, string>()
+{
+    [1] = "Понельник",
+    [2] = "Вторник",
+    [3] = "Среда",
+    [4] = "Четверг",
+    [5] = "Пятница",
+    [6] = "Суббота",
+    [7] = "Воскресенье",
+};
+Console.WriteLine("inpun number 1-7: ");
+string x = Console.ReadLine();
+int x_int = Convert.ToInt32(x);
+Console.WriteLine(dict[x_int]);
