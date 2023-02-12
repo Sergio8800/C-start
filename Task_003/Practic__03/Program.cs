f46aad7771b06d4b1a6ebbd75e36d39f334e1d57
Sergio8800 <wasp8800@gmail.com>
2023-02-12 23:41:02 +0100

    Task function

---
+++ Task_003/Practic__03/Program.cs
@@ -0,0 +1,33 @@
+﻿
+void TwooNumber(double A, double B)
+{
+    double result = Math.Pow(A, B);
+    Console.WriteLine($" {A} to the power {B} = {result}");
+    
+}
+TwooNumber(2, 3);
+// next-------------------
+void SummaDigit(int Number)
+{
+    int summa = 0;
+    while (Number > 0)
+    {
+        summa = summa + Number % 10;
+        Number= Number / 10;
+    }
+    Console.WriteLine($"Summa digits of number is {summa}");
+}
+SummaDigit(22);
+void MassivNumb(int Number)
+{
+    int[] mass = new int[Number];
+    Console.WriteLine($"Array of {Number} element: ");
+    while (Number > 0)
+    {
+        int x = new Random().Next(10,20);
+        mass[Number-1] = x;
+        Console.Write(mass[Number-1] + " ");
+        Number--;
+    }
+}
+MassivNumb(8);
